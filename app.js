92d3d494fbdffc24507db1ce4dcf41e9b9d1dbf9
leicaflorian <florian.leica@gmail.com>
2019-10-19 21:50:52 +0200

    Changed from cli to normal start

--- app.js
+++ app.js
@@ -1,31 +1,42 @@
 'use strict'
 
 const path = require('path')
+const Fastify = require('fastify')
 const AutoLoad = require('fastify-autoload')
 
-const port     = process.env.PORT || '3000';
-const env      = process.env.NODE_ENV || 'development';
+const port = process.env.PORT || '3000';
+const env = process.env.NODE_ENV || 'development';
 
-module.exports = function (fastify, opts, next) {
-  // Place here your custom code!
-
-  // Do not touch the following lines
-
-  // This loads all plugins defined in plugins
-  // those should be support plugins that are reused
-  // through your application
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'plugins'),
-    options: Object.assign({}, opts)
-  })
-
-  // This loads all plugins defined in services
-  // define your routes in one of these
-  fastify.register(AutoLoad, {
-    dir: path.join(__dirname, 'services'),
-    options: Object.assign({}, opts)
-  })
-
-  // Make sure to call next when done
-  next()
+const opts = {
+  logger: true
 }
+
+const fastify = Fastify(opts)
+
+
+//module.exports = function (fastify, opts, next) {
+// Place here your custom code!
+
+// Do not touch the following lines
+
+// This loads all plugins defined in plugins
+// those should be support plugins that are reused
+// through your application
+fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'plugins'),
+  options: Object.assign({}, opts)
+})
+
+// This loads all plugins defined in services
+// define your routes in one of these
+fastify.register(AutoLoad, {
+  dir: path.join(__dirname, 'services'),
+  options: Object.assign({}, opts)
+})
+
+// Make sure to call next when done
+//  next()
+
+//}
+
+fastify.listen(port)
