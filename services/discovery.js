3a7aa9e26e87878cdbdbc4bf1206a09b43825f2c
leicaflorian <florian.leica@gmail.com>
2019-10-31 21:27:23 +0100

    Aggioranto Accesso discovery

--- services/discovery.js
+++ services/discovery.js
@@ -1,29 +1,40 @@
 const axios = require('axios').default;
 const mainURL = `https://it.dplay.com/ajax/playbackjson/channel/`;
 const channelsMap = {
-	nove: "3",
-	realtime: "2",
-	dmax: "5",
-	giallo: "6",
-	k2: "7",
-	frisbee: "8",
+  nove: "3",
+  realtime: "2",
+  dmax: "5",
+  giallo: "6",
+  k2: "7",
+  frisbee: "8",
 }
 
 module.exports = function(fastify, opts, next) {
 
   fastify.get('/discovery/:channel', function(request, reply) {
-		console.log(mainURL + channelsMap[request.params.channel]);
-		
-    axios.get(mainURL + channelsMap[request.params.channel])
+    console.log(mainURL + channelsMap[request.params.channel]);
+
+    axios.get(mainURL + channelsMap[request.params.channel], {
+        withCredentials: true,
+        headers: {
+          Referer: "https://it.dplay.com/nove/",
+          "X-Requested-With": "XMLHttpRequest",
+          "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.70 Safari/537.36",
+          "Sec-Fetch-Site": "same-origin",
+          "Sec-Fetch-Mode": "cors"
+        }
+
+      })
       .then(resp => {
-				const jsonData = JSON.parse(resp.data);
+        const jsonData = JSON.parse(resp.data);
 
         reply.redirect(jsonData.data.attributes.streaming.hls.url)
-			})
-			.catch(er => {
-				console.log(er.message);
-				
-			})
+      })
+      .catch(er => {
+        console.log(er.message);
+
+        reply.send(er);
+      })
   })
 
 
