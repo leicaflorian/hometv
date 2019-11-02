e522fd43a445204e9785ace31235291fbfcb8110
leicaflorian <florian.leica@gmail.com>
2019-11-02 11:54:24 +0100

    Added Homepage

--- plugins/support.js
+++ plugins/support.js
@@ -1,14 +1,28 @@
 'use strict'
 
-const fp = require('fastify-plugin')
+const fp = require('fastify-plugin');
+const resolve = require('path').resolve
 
 // the use of fastify-plugin is required to be able
 // to export the decorators to the outer scope
 
-module.exports = fp(function (fastify, opts, next) {
-  fastify.decorate('someSupport', function () {
+module.exports = fp(function(fastify, opts, next) {
+  fastify.decorate('someSupport', function() {
     return 'hugs'
   })
+
+  fastify.register(require('point-of-view'), {
+    engine: {
+      pug: require('pug'),
+    },
+    includeViewExtension: true,
+    templates: 'views',
+    options: {
+      layout: './views/template.pug',
+      filename: resolve('views')
+    }
+  })
+
   next()
 })
 
--- services/root.js
+++ services/root.js
@@ -1,10 +1,27 @@
 'use strict'
+const fs = require("fs");
 const axios = require('axios');
+const m3u8Parser = require("iptv-playlist-parser");
+
+async function getListaIptv() {
+  const content = await fs.readFileSync('assets/lista_iptv.m3u', 'utf8');
+
+  return content;
+}
 
 module.exports = function(fastify, opts, next) {
-  fastify.get("/", function(request, reply) {
+  fastify.get("/", async function(request, reply) {
+    const parsedManifest = m3u8Parser.parse(await getListaIptv());
+
+    console.log(parsedManifest);
+    
+
+    reply.view("index", { data: parsedManifest });
+  })
+
+  fastify.get("/list", async function(request, reply) {
     const fs = require('fs')
-    const stream = fs.readFileSync('assets/lista_iptv.m3u', 'utf8')
+    const stream = await getListaIptv()
 
     //reply.header("Content-Type", "application/vnd.apple.mpegurl");
 
