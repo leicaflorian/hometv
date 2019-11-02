6abf6d2b3eeeb84c02ee0f6ca9d7b6e087a80183
leicaflorian <florian.leica@gmail.com>
2019-11-02 10:15:17 +0100

    Update Home

--- services/root.js
+++ services/root.js
@@ -4,7 +4,9 @@ const axios = require('axios');
 module.exports = function(fastify, opts, next) {
   fastify.get("/", function(request, reply) {
     const fs = require('fs')
-    const stream = fs.createReadStream('assets/lista_iptv.m3u', 'utf8')
+    const stream = fs.readFileSync('assets/lista_iptv.m3u', 'utf8')
+
+    //reply.header("Content-Type", "application/vnd.apple.mpegurl");
 
     reply.send(stream)
   })
