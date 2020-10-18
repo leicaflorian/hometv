33bcbc8ead809ab515096c35300665618f466f27
Leica Florian Robert <florian.leica@gmail.com>
2020-10-18 19:55:00 +0200

    fixed rai and sky problems

--- services/sky.js
+++ services/sky.js
@@ -1,5 +1,7 @@
 const axios = require('axios').default;
-const mainURL = `https://video.sky.it/be/getLivestream?id=`;
+const {template: _template} = require("lodash")
+//const mainURL = `https://video.sky.it/be/getLivestream?id=`;
+const mainURL = "https://apid.sky.it/vdp/v1/getLivestream?id=${id}&isMobile=false";
 const channelsMap = {
 	tv8: "7",
 	skyarte: "8",
@@ -7,15 +9,16 @@ const channelsMap = {
 	tg24: "1",
 }
 
-module.exports = function(fastify, opts, next) {
+module.exports = function (fastify, opts, next) {
 
-  fastify.get('/sky/:channel', function(request, reply) {
-    axios.get(mainURL + channelsMap[request.params.channel])
-      .then(resp => {
-        reply.redirect(resp.data.streaming_url)
-      })
-  })
+	fastify.get('/sky/:channel', async function (request, reply) {
+		const url = _template(mainURL)
+
+		const resp = await axios.get(url({id: channelsMap[request.params.channel]}))
+
+		reply.redirect(resp.data.streaming_url)
+	})
 
 
-  next()
+	next()
 }
