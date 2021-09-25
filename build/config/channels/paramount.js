954eeb9b60d669c620c8636c3b1087f66c8e22d5
leicaflorian <florian.leica@gmail.com>
2021-09-25 15:48:44 +0200

    removes build folder

--- build/config/channels/paramount.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    { id: 'spike', code: '829515', name: 'Spike', tvgLogo: '441', tvgName: '', tvgId: '' },
-    { id: 'paramount', code: '195657', name: 'Paramount Channel', tvgLogo: '299', tvgName: '', tvgId: '' },
-];
-//# sourceMappingURL=paramount.js.map
--- build/config/channels/sky.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.default = [
-    { id: 'tv8', code: '7', name: 'TV 8', tvgLogo: '237', tvgName: '', tvgId: '' },
-    { id: 'cielo', code: '2', name: 'Cielo', tvgLogo: '239', tvgName: '', tvgId: '' },
-    { id: 'arte', code: '8', name: 'Sky Arte', tvgLogo: '11', tvgName: '', tvgId: '' },
-    { id: 'tg24', code: '1', name: 'Sky Tg24', tvgLogo: '240', tvgName: '', tvgId: '' },
-];
-//# sourceMappingURL=sky.js.map
