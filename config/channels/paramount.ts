23b5d9a6b83a40d28fc596dfe963dc4aed2131a9
leicaflorian <florian.leica@gmail.com>
2021-10-16 00:46:00 +0200

    Fixes epg name

--- config/channels/paramount.ts
+++ config/channels/paramount.ts
@@ -1,7 +1,29 @@
 import {Channel} from "../../types/ChannelsList";
 
 export default [
-  { id: 'spike', code: '829515', name: 'Spike', tvgLogo: '441', tvgName: '', tvgId: '' },
-  { id: 'paramount', code: '195657', name: 'Paramount Channel', tvgLogo: '299', tvgName: '', tvgId: '' },
-  { id: 'vh1', code: '195657', name: 'VH1', tvgLogo: 'https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/VH1_logonew.svg/1200px-VH1_logonew.svg.png', tvgName: '', tvgId: '' },
-]  as Channel[]
+  {
+    id: "spike",
+    code: "829515",
+    name: "Spike",
+    tvgLogo: "441",
+    tvgName: "spike tv",
+    tvgId: "",
+  },
+  {
+    id: "paramount",
+    code: "195657",
+    name: "Paramount Channel",
+    tvgLogo: "299",
+    tvgName: "",
+    tvgId: "",
+  },
+  {
+    id: "vh1",
+    code: "195657",
+    name: "VH1",
+    tvgLogo:
+      "https://upload.wikimedia.org/wikipedia/commons/thumb/9/94/VH1_logonew.svg/1200px-VH1_logonew.svg.png",
+    tvgName: "",
+    tvgId: "",
+  },
+] as Channel[];
